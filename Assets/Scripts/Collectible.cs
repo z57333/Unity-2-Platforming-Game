73dbb1cb5234686c5d07a2d0617b2fd897f6adbb
z57333 <73370638+z57333@users.noreply.github.com>
2020-11-06 18:25:32 -0800

    created Collectible abstract class
    Zack Wu

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Collectible : MonoBehaviour
+{
+    public string CollectibleName;
+    public string description;
+    public GameObject player;
+
+    public abstract void Use();
+}
